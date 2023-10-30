ad4226ade8c37ab16609bbbee339c8059abe1530
Charlie <u202120841@upc.edu.pe>
2023-10-30 16:07:16 -0500

    fea: implememt Galery Controller

---
+++ FastService.API/FastService/Controllers/GalleryController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using FastService.API.FastService.Domain.Models;
+using FastService.API.FastService.Domain.Services;
+using FastService.API.FastService.Resources;
+using FastService.API.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastService.API.FastService.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class GalleryController : ControllerBase
+{
+    private readonly IGalleryService _galleryService;
+    private readonly IMapper _mapper;
+
+    public GalleryController(IGalleryService galleryService, IMapper mapper)
+    {
+        _galleryService = galleryService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<GalleryResource>> GetAllAsync()
+    {
+        var galleries = await _galleryService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Gallery>, IEnumerable<GalleryResource>>(galleries);
+
+        return resources;
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveGalleryResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+        
+        var Gallery =  _mapper.Map<SaveGalleryResource,Gallery>(resource);
+
+        var result = await _galleryService.SaveAsync(Gallery);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var GalleryResource = _mapper.Map<Gallery, GalleryResource>(result.Resource);
+
+        return Ok(GalleryResource);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveGalleryResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+
+        var Gallery = _mapper.Map<SaveGalleryResource, Gallery>(resource);
+
+        var result = await _galleryService.UpdateAsync(id, Gallery);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var GalleryResource = _mapper.Map<Gallery, GalleryResource>(result.Resource);
+
+        return Ok(GalleryResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _galleryService.DeleteAsync(id);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var GalleryResource = _mapper.Map<Gallery, GalleryResource>(result.Resource);
+
+        return Ok(GalleryResource);
+    }
+
+}
--- FastService.API/FastService/Resources/GalleryResource.cs
+++ FastService.API/FastService/Resources/GalleryResource.cs
@@ -7,4 +7,5 @@ public class GalleryResource
     public int Id { get; set; }
     public string ImgUrl { get; set; }
     public ExpertResource Expert { get; set; }
+    
 }
